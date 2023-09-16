f3edf668a84c3f381a998e3889479e021c175bd6
Matheus Nascimento <matthns98@gmail.com>
2023-09-16 11:03:10 -0300

    layout: creating new layout based on figma model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,38 +14,68 @@ const rhodium = Rhodium_Libre({
 });
 
 export default function Home() {
+  type MenuItem = {
+    id: string;
+    label: string;
+    href: string;
+  };
+
+  const menu: MenuItem[] = [
+    {
+      id: "1",
+      label: "home",
+      href: "/",
+    },
+    {
+      id: "2",
+      label: "about",
+      href: "/about",
+    },
+    {
+      id: "3",
+      label: "contact",
+      href: "/contact",
+    },
+    {
+      id: "4",
+      label: "tools",
+      href: "/tools",
+    },
+  ];
+
+  type SocialItem = {
+    id: string;
+    label: string;
+    icon: any;
+    href: string;
+  };
+
+  const social: SocialItem[] = [
+    {
+      id: "1",
+      label: "github",
+      icon: <AiFillGithub />,
+      href: "https://github.com/matthns/",
+    },
+    {
+      id: "2",
+      label: "linkedin",
+      icon: <BiLogoLinkedin />,
+      href: "https://www.linkedin.com/in/matthns/",
+    },
+    {
+      id: "3",
+      label: "my business",
+      icon: <BiSolidBusiness />,
+      href: "https://mmadevs.com.br",
+    },
+  ];
+
   const [user, setUser] = useState<{ name: string; avatar_url: string }>({
     name: "",
     avatar_url: "",
   });
   const [loading, setLoading] = useState(true);
-
-  //   api
-  //     .get(`/users/matthns`)
-  //     .then((response) => setUser(response.data))
-  //     .catch((err) => {
-  //       console.error("API GET error" + err);
-  //     });
-  //   setLoading(false);
-  // }, []);
-
-  // useEffect(() => {
-  //   async function loadProfile() {
-  //     const [response] = await Promise.all([api.get(`/users/matthns`)]);
-  //     setUser(response.data);
-  //   }
-  //   loadProfile();
-  //   setLoading(false);
-  // }, []);
-
-  // useEffect(() => {
-  //   async function loadProfile() {
-  //     const [profileData] = await Promise.all([api.get(`/users/matthns`)]);
-  //     setUser(profileData.data);
-  //   }
-  //   loadProfile();
-  //   setLoading(false);
-  // }, []);
 
   useEffect(() => {
     async function loadProfile() {
@@ -73,71 +103,70 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-[#000413] w-screen h-screen p-10 text-white">
-      <div className="flex flex-col items-center mt-4 p-4">
-        <h2
-          className={`${rhodium.className} text-center text-3xl mb-6 hover:underline cursor-pointer`}
-        >
-          {user?.name.toLowerCase()}
-        </h2>
-        <Image
-          className={`rounded-full mb-8 border-4 border-blue-300`}
-          src={user?.avatar_url}
-          alt="My profile photo"
-          width={300}
-          height={300}
-        />
-        <div className="">
-          <p className="font-bold text-2xl p-2">Hi! My name is {user?.name}</p>
-          <p className="text-xl p-2">I`m a Front-end Developer</p>
-          <p className="text-xl p-2">
-            Currently I`m an associate at {""}
-            <strong className="hover:bg-white hover:p-1 hover:rounded-md hover:text-black">
-              <Link href={"https://mmadevs.com.br"} target="_blank">
-                {" "}
-                MMA Devs
-              </Link>
-            </strong>
-          </p>
-          <ul className="list-disc ml-8 mt-4">
-            <li className="text-sm">
-              Graduated in System Analysis and Development (Unicesumar)
-            </li>
-            <li>Front-end developer (Ada Tech)</li>
+    <div className="bg-[#000413] w-screen h-screen p-10 text-white flex justify-center">
+      <div className="max-w-screen-2xl w-full h-full">
+        <nav className={`${rhodium.className} flex justify-between`}>
+          <h1 className={`text-3xl hover:underline cursor-pointer`}>
+            <Link href={"/"}>{user?.name.toLowerCase()}</Link>
+          </h1>
+          <ul className="flex flex-row gap-6 text-xl">
+            {menu.map((menuItem) => {
+              return (
+                <li
+                  key={menuItem.id}
+                  className="transition ease-in-out delay-150 hover:scale-110 hover:duration-300 hover:text-blue-400"
+                >
+                  <Link href={menuItem.href}>{menuItem.label}</Link>
+                </li>
+              );
+            })}
           </ul>
-          <div className="mt-6 flex flex-row gap-4">
-            <Link href={"https://github.com/matthns/"} target="_blank">
-              <AiFillGithub
-                size={40}
-                className={
-                  "transition ease-in-out delay-150 hover:scale-110 hover: duration-300"
-                }
-              />
-            </Link>
-            <Link href={"https://www.linkedin.com/in/matthns/"} target="_blank">
-              <BiLogoLinkedin
-                size={40}
-                className={
-                  "transition ease-in-out delay-150 hover:scale-110 hover: duration-300"
-                }
-              />
-            </Link>
-            <Link href={"https://mmadevs.com.br"} target="_blank">
-              <BiSolidBusiness
-                size={40}
-                className={
-                  "transition ease-in-out delay-150 hover:scale-110 hover: duration-300"
-                }
-              />
-            </Link>
-            <Link href={"mailto:matthns98@gmail.com"} target="_blank">
-              <ImMail4
-                size={40}
-                className={
-                  "transition ease-in-out delay-150 hover:scale-110 hover: duration-300"
-                }
-              />
-            </Link>
+        </nav>
+        <div className="flex items-center justify-center h-full">
+          <div className="flex gap-12 p-4">
+            <Image
+              className={`rounded-full mb-8 border-4 border-[#152544]`}
+              src={user?.avatar_url}
+              alt="My profile photo"
+              width={300}
+              height={300}
+            />
+            <div className="">
+              <p className="font-bold text-2xl p-2">
+                Hi! My name is {user?.name}
+              </p>
+              <p className="text-xl p-2">I`m a Front-end Developer</p>
+              <p className="text-xl p-2">
+                Currently I`m an associate at {""}
+                <strong className="hover:bg-white hover:p-1 hover:rounded-md hover:text-black">
+                  <Link href={"https://mmadevs.com.br"} target="_blank">
+                    {" "}
+                    MMA Devs
+                  </Link>
+                </strong>
+              </p>
+              <ul className="list-disc ml-8 mt-4">
+                <li className="text-sm">
+                  Graduated in System Analysis and Development (Unicesumar)
+                </li>
+                <li>Front-end developer (Ada Tech)</li>
+              </ul>
+              <ul className="flex flex-row gap-4 mt-4">
+                {social.map((socialItem) => {
+                  return (
+                    <li
+                      key={socialItem.id}
+                      className={`text-4xl transition ease-in-out delay-150 hover:scale-110 hover:duration-300 p-3 bg-[#152544] rounded-xl`}
+                    >
+                      <Link href={socialItem.href} target="_blank">
+                        {socialItem.icon}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="mt-6 flex flex-row gap-4"></div>
+            </div>
           </div>
         </div>
       </div>
